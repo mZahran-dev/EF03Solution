93e3ad90f8db8eccec3eea4b6b71fbf543710930
Mahmoud Zahran <mi4654110@gmail.com>
2024-08-22 12:10:44 +0300

    feat : Adding OneToMany Relationship Between Topic and Courses

--- EF03/ITI DB Schema/Course.cs
+++ EF03/ITI DB Schema/Course.cs
@@ -12,7 +12,9 @@ namespace EFC01.ITI_DB_Schema
         public int Duration { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
-
+        
         public ICollection<Student_Course> student_Courses { get; set; } = new HashSet<Student_Course>();
+        
+        public ICollection<Topic> topic_Courses { get; set; } = new HashSet<Topic>();
     }
 }
