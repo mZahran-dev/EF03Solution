7b133d515bba8ef45b433ba7a413caaf27d425f8
Mahmoud Zahran <mi4654110@gmail.com>
2024-08-22 12:14:41 +0300

    Rename : student_Courses To CourseStudent in Course Class

--- EF03/ITI DB Schema/Course.cs
+++ EF03/ITI DB Schema/Course.cs
@@ -13,7 +13,7 @@ namespace EFC01.ITI_DB_Schema
         public string Name { get; set; }
         public string Description { get; set; }
         
-        public ICollection<Student_Course> student_Courses { get; set; } = new HashSet<Student_Course>();
+        public ICollection<Student_Course> CourseStudent { get; set; } = new HashSet<Student_Course>();
         
         public ICollection<Topic> topic_Courses { get; set; } = new HashSet<Topic>();
     }
